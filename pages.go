383a14cf993f2f11d0f7e2cc3431145a950aaffd
rorycl <rorycl@campbell-lange.net>
2025-10-23 21:00:04 +0100

    pages: improve page definition checking, add tests

--- pages.go
+++ pages.go
@@ -10,24 +10,98 @@ import (
 	"text/template"
 )
 
+// ErrInvalidConfig reports an invalid yaml configuration file, although
+// one that passed parsing.
+type ErrInvalidConfig struct {
+	info string
+}
+
+// Error reports the error.
+func (e ErrInvalidConfig) Error() string {
+	return fmt.Sprintf("invalid yaml configuration: %s", e.info)
+}
+
 // config describes the config in a yaml configuration file
 type config struct {
 	PageTemplate string `yaml:"pageTemplate"`
 	Pages        []page `yaml:"pages"`
 }
 
+func (c *config) validateConfig() error {
+	if c.PageTemplate == "" {
+		return ErrInvalidConfig{"pageTemplate not set"}
+	}
+	if len(c.Pages) < 2 {
+		return ErrInvalidConfig{"at least two pages must be defined"}
+	}
+
+	// Register of page and zone urls to ensure that the latter only
+	// call out valid pages. Note that the context of a page is stored
+	// in the zoneURLMap value and if multiple incorrect Zone Target
+	// values are used with the same URL the last will be reported.
+	urlMap := map[string]bool{}
+	zoneURLMap := map[string]string{}
+
+	for ii, pg := range c.Pages {
+		if pg.URL == "" {
+			return ErrInvalidConfig{fmt.Sprintf("url empty for page %d (%s)", ii, pg.Title)}
+		}
+		urlMap[pg.URL] = true
+		if pg.Title == "" {
+			return ErrInvalidConfig{fmt.Sprintf("title empty for page %d (%s)", ii, pg.URL)}
+		}
+		if pg.ImagePath == "" {
+			return ErrInvalidConfig{fmt.Sprintf("image path empty for page %d (%s)", ii, pg.Title)}
+		}
+		if len(pg.Zones) < 1 {
+			return ErrInvalidConfig{fmt.Sprintf("no zones defined for page %d (%s)", ii, pg.Title)}
+		}
+		for zi, zo := range pg.Zones {
+			if zo.Target == "" {
+				return ErrInvalidConfig{fmt.Sprintf(
+					"page %d zone %d empty 'Target' value",
+					ii, zi,
+				)}
+			}
+			zoneURLMap[zo.Target] = fmt.Sprintf(
+				"page %d zone %d", ii, zi,
+			)
+			if zo.Right < zo.Left || zo.Right == 0 {
+				return ErrInvalidConfig{fmt.Sprintf(
+					"page %d zone %d invalid 'Right' value of %d",
+					ii, zi, zo.Right,
+				)}
+			}
+			if zo.Bottom < zo.Top || zo.Bottom == 0 {
+				return ErrInvalidConfig{fmt.Sprintf(
+					"page %d zone %d invalid 'Bottom' value of %d",
+					ii, zi, zo.Bottom,
+				)}
+			}
+		}
+	}
+
+	// Validate urls.
+	for k, v := range zoneURLMap {
+		if _, ok := urlMap[v]; !ok {
+			return ErrInvalidConfig{fmt.Sprintf(
+				"invalid Zone Target URL %s doesn't point to a page (%s)",
+				k, v,
+			)}
+		}
+	}
+	return nil
+}
+
 // newConfig creates and validates a new config from reading a yaml
 // file.
-func newConfig(path string) (*config, error) {
-	r, err := os.ReadFile(path)
-	if err != nil {
-		return nil, fmt.Errorf("config '%q' reading error: %v", err)
-	}
+func newConfig(b []byte) (*config, error) {
 	var c config
-	if err = yaml.Unmarshal(r, &c); err != nil {
+	if err := yaml.Unmarshal(b, &c); err != nil {
 		return nil, fmt.Errorf("unmarshal error: %v", err)
 	}
-	return &c, nil
+	err := c.validateConfig()
+	return &c, err
 }
 
 // pageZone sets up a rectangular page zone on a page that, when
---
+++ pages_test.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		config string
+		err    error
+	}{
+		{
+			name: "ok",
+			err:  nil,
+			config: `
+---
+pageTemplate: "templates/page.html"
+pages:
+  -
+    URL: "/home"
+    Title: "Home"
+    ImagePath: "images/home.jpg"
+    Zones:
+      -
+        Left:   367
+        Top:    44
+        Right:  539
+        Bottom: 263
+        Target: "/detail"
+  -
+    URL: "/detail"
+    Title: "Detail"
+    ImagePath: "images/detail.jpg"
+    Zones:
+      -
+        Left: 436
+        Top:  31
+        Right: 538
+        Bottom: 73
+        Target: "/home"
+`},
+
+		{
+			name: "yaml parsing error",
+			err:  errors.New("yaml parsing error"),
+			config: `
+---
+pageTemplate: "templates/page.html"
+pages:
+    URL: "/home"
+    Title: "Home"
+    ImagePath: "images/home.jpg"
+    Zones:
+`},
+
+		{
+			name: "too few pages",
+			err:  ErrInvalidConfig{"too few pages"},
+			config: `
+---
+pages:
+  -
+    URL: "/home"
+    Title: "Home"
+    ImagePath: "images/home.jpg"
+    Zones:
+      -
+        Left:   367
+        Top:    44
+        Right:  539
+        Bottom: 263
+        Target: "/detail"
+`},
+
+		{
+			name: "too few zones",
+			err:  ErrInvalidConfig{"too few zones"},
+			config: `
+---
+pageTemplate: "templates/page.html"
+pages:
+  -
+    URL: "/home"
+    Title: "Home"
+    ImagePath: "images/home.jpg"
+    Zones:
+  -
+    URL: "/detail"
+    Title: "Detail"
+    ImagePath: "images/detail.jpg"
+    Zones:
+      -
+        Left: 436
+        Top:  31
+        Right: 538
+        Bottom: 73
+        Target: "/home"
+`},
+		{
+			name: "invalid zone url",
+			err:  ErrInvalidConfig{"invalid zone url"},
+			config: `
+---
+pageTemplate: "templates/page.html"
+pages:
+  -
+    URL: "/home"
+    Title: "Home"
+    ImagePath: "images/home.jpg"
+    Zones:
+      -
+        Left:   367
+        Top:    44
+        Right:  539
+        Bottom: 263
+        Target: "/detail"
+  -
+    URL: "/detail"
+    Title: "Detail"
+    ImagePath: "images/detail.jpg"
+    Zones:
+      -
+        Left: 436
+        Top:  31
+        Right: 538
+        Bottom: 73
+        Target: "/homes"
+`},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := newConfig([]byte(tt.config))
+			if err != nil {
+				if tt.err == nil {
+					t.Fatalf("unexpected error: %v", err)
+				}
+				var e ErrInvalidConfig
+				if errors.As(tt.err, &e) {
+					if !errors.As(err, &e) {
+						t.Fatalf("expected ErrInvalidConfig, got %v", err)
+					}
+				}
+			}
+			if err == nil && tt.err != nil {
+				t.Fatalf("expected error: %T (%v)", tt.err, tt.err)
+			}
+		})
+	}
+}
