435c0a30244db568507eb82be36ab64bff2d1e4d
rorycl <rorycl@campbell-lange.net>
2025-10-23 18:04:08 +0100

    main: add flag parsing and tests

--- main.go
+++ main.go
@@ -1,26 +1,103 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"net"
 	"os"
+	"strconv"
 )
+
+var usage = `
+run a "firstgo" webserver to show interactive image "pages".
+
+This programme uses the configuration in config.yaml and images in the
+images directory and css in the static directory to serve up interactive
+image "web pages" to mock up a web site or service.
+
+eg ./firstgo [-address 192.168.4.5] [-port 8001] <configfile>
+`
+
+var ErrFlagExited error = errors.New("an error occurred")
+
+// flagGet checks the flags
+func flagGet() (string, string, string, error) {
+
+	var address, port, configFile string
+	flag.StringVar(&address, "address", "127.0.0.1", "server network address")
+	flag.StringVar(&port, "port", "8000", "server network port")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintln(
+			flag.CommandLine.Output(),
+			"  <configFile>\n    	yaml configuration file")
+		fmt.Fprint(flag.CommandLine.Output(), usage)
+	}
+
+	flag.Parse()
+	if address == "" || port == "" {
+		flag.Usage()
+		return "", "", "", ErrFlagExited
+	}
+	if len(flag.Args()) != 1 {
+		flag.Usage()
+		return "", "", "", ErrFlagExited
+	}
+	configFile = flag.Args()[0]
+
+	// check validity of fields
+	a := net.ParseIP(address)
+	if a == nil {
+		return "", "", "", fmt.Errorf("address %s invalid IP address\n", address)
+	}
+	_, err := strconv.Atoi(port)
+	if err != nil {
+		return "", "", "", fmt.Errorf("port %s invalid\n", port)
+	}
+	if _, err = os.Stat(configFile); err != nil {
+		return "", "", "", fmt.Errorf("config file %s could not be found", configFile)
+	}
+	return address, port, configFile, nil
+}
+
+// Serve indirects serve for testing
+var Serve func(*server) error = (*server).serve
+
+var Exiter func(int) = os.Exit
 
 func main() {
 
-	config, err := newConfig("config.yaml")
+	address, port, configFile, err := flagGet()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		if !errors.Is(err, ErrFlagExited) {
+			fmt.Println(err)
+		}
+		Exiter(1)
+		return
 	}
 
-	server, err := newServer(config.Pages, config.PageTemplate)
+	config, err := newConfig(configFile)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
-	}
-	err = server.serve()
-	if err != nil {
-		fmt.Println(err)
+		Exiter(1)
+		return
 	}
 
+	server, err := newServer(address, port, config.Pages, config.PageTemplate)
+	if err != nil {
+		fmt.Println(err)
+		Exiter(1)
+		return
+	}
+	err = Serve(server)
+	// err = server.serve()
+	if err != nil {
+		fmt.Println(err)
+		Exiter(1)
+		return
+	}
+	Exiter(0)
 }
---
+++ main_test.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestMainFlags(t *testing.T) {
+
+	tests := []struct {
+		args       []string
+		address    string
+		port       string
+		configFile string
+		err        error
+	}{
+		{
+			args:       []string{"prog", "-address", "10.0.1.1", "-port", "8021", "config.yaml"},
+			address:    "10.0.1.1",
+			port:       "8021",
+			configFile: "config.yaml",
+			err:        nil,
+		},
+		{
+			args:       []string{"prog", "config.yaml"},
+			address:    "127.0.0.1",
+			port:       "8000",
+			configFile: "config.yaml",
+			err:        nil,
+		},
+		{
+			args: []string{"prog"},
+			err:  ErrFlagExited,
+		},
+		{
+			args: []string{"prog", "-address", "10.2000.1.1", "config.yaml"},
+			err:  errors.New("invalid ip"),
+		},
+		{
+			args: []string{"prog", "-port", "hi", "config.yaml"},
+			err:  errors.New("invalid port"),
+		},
+		{
+			args: []string{"prog", "no config.yaml"},
+			err:  errors.New("no config file found"),
+		},
+	}
+
+	for ii, tt := range tests {
+		t.Run(fmt.Sprintf("test_%d", ii), func(t *testing.T) {
+
+			flag.CommandLine = flag.NewFlagSet(fmt.Sprintf("%d", ii), flag.ContinueOnError)
+			os.Args = tt.args
+			address, port, configFile, err := flagGet()
+
+			if tt.err == nil && err != nil {
+				t.Fatalf("unexpected err %v", err)
+			}
+			if tt.err != nil && errors.Is(err, tt.err) {
+				return
+			}
+			if tt.err != nil && err != nil {
+				// assume ok
+				t.Logf("got err %v (expected %v)", err, tt.err)
+				return
+			}
+			if got, want := address, tt.address; got != want {
+				t.Errorf("address got %v want %v", got, want)
+			}
+			if got, want := port, tt.port; got != want {
+				t.Errorf("port got %v want %v", got, want)
+			}
+			if got, want := configFile, tt.configFile; got != want {
+				t.Errorf("configFile got %v want %v", got, want)
+			}
+		})
+	}
+}
+
+func TestMainMain(t *testing.T) {
+
+	var exitCode int
+	Exiter = func(e int) {
+		exitCode = e
+	}
+
+	Serve = func(*server) error { return nil }
+	flag.CommandLine = flag.NewFlagSet("test-main", flag.ContinueOnError)
+	os.Args = []string{"prog", "config.yaml"}
+	address, port, configFile, err := flagGet()
+	if err != nil {
+		t.Fatalf("flag parse error at step %s, %v", "one", err)
+	}
+
+	config, err := newConfig(configFile)
+	if err != nil {
+		t.Fatalf("config file error at step %s, %v", "one", err)
+	}
+
+	server, err := newServer(address, port, config.Pages, config.PageTemplate)
+	if err != nil {
+		t.Fatalf("new server error at step %s, %v", "one", err)
+	}
+
+	err = Serve(server)
+	if err != nil || exitCode != 0 {
+		t.Fatalf("serve error at step %s, %v : code %d", "one", err, exitCode)
+	}
+
+}
