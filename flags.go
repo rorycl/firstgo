7fb1503a61eb17a47337ef2720defe467f021e87
rorycl <rorycl@campbell-lange.net>
2025-10-25 00:19:40 +0100

    flags: create subcommands with injectable app

--- flags.go
+++ flags.go
@@ -9,6 +9,14 @@ import (
 	"github.com/jessevdk/go-flags"
 )
 
+// Applicator is an interface to the central coordinator for the project
+// (concretely provided by App in app.go) to allow for testing.
+type Applicator interface {
+	Serve(address, port, configFile string) error
+	Init(directory string) error
+	Demo(address, port string) error
+}
+
 // FlagCustomError is a custom flag parsing error.
 type FlagCustomError struct {
 	message string
@@ -20,9 +28,7 @@ func (f FlagCustomError) Error() string {
 }
 
 // Options is the global flag options wrapper struct.
-type Options struct {
-	// can we add "serve command" etc in here?
-}
+type Options struct{}
 
 // ServeCommand are the flag options for the '<prog> serve' command.
 type ServeCommand struct {
@@ -32,7 +38,7 @@ type ServeCommand struct {
 		ConfigFile string `description:"configuration yaml file"`
 	} `positional-args:"yes" required:"yes"`
 	// injected main app
-	App *App `no-flag:"true"`
+	App Applicator `no-flag:"true"`
 }
 
 // validate runs additional checks on the ServeCommand.
@@ -62,19 +68,32 @@ func (c *ServeCommand) Execute(args []string) error {
 type InitCommand struct {
 	Directory string `short:"d" long:"directory" description:"directory to write files" default:"cwd"`
 	// injected main app
-	App *App `no-flag:"true"`
+	App Applicator `no-flag:"true"`
 }
 
 // Execute runs the Init process.
 func (c *InitCommand) Execute(args []string) error {
+	err := c.validate()
+	if err != nil {
+		return err
+	}
+	return c.App.Init(c.Directory)
+}
+
+// validate runs additional checks on the InitCommand.
+func (c *InitCommand) validate() error {
 	var err error
 	if c.Directory == "cwd" {
 		c.Directory, err = os.Getwd()
 		if err != nil {
-			return fmt.Errorf("could not get current working directory %v", err)
+			return FlagCustomError{fmt.Sprintf("could not get current working directory %v", err)}
 		}
 	}
-	return c.App.Init(c.Directory)
+	dir, err := os.Stat(c.Directory)
+	if err != nil || !dir.IsDir() {
+		return FlagCustomError{fmt.Sprintf("directory %s not found", c.Directory)}
+	}
+	return nil
 }
 
 // DemoCommand are the flag options for the '<prog> demo' command.
@@ -82,16 +101,34 @@ type DemoCommand struct {
 	Address string `short:"a" long:"address" description:"server address" default:"127.0.0.1"`
 	Port    string `short:"p" long:"port" description:"server port" default:"8000"`
 	// injected main app
-	App *App `no-flag:"true"`
+	App Applicator `no-flag:"true"`
 }
 
 // Execute runs the Demo process.
 func (c *DemoCommand) Execute(args []string) error {
+	err := c.validate()
+	if err != nil {
+		return err
+	}
 	return c.App.Demo(c.Address, c.Port)
 }
 
-var cmdTpl string = `
+// validate runs additional checks on the DemoCommand.
+func (c *DemoCommand) validate() error {
+	if a := net.ParseIP(c.Address); a == nil {
+		return FlagCustomError{
+			fmt.Sprintf("address %s is an invalid IP address", c.Address),
+		}
+	}
+	if _, err := strconv.Atoi(c.Port); err != nil {
+		return FlagCustomError{
+			fmt.Sprintf("port %s invalid", c.Port),
+		}
+	}
+	return nil
+}
 
+var cmdTpl string = `
 A web server for prototyping web interfaces using sketches and clickable
 zones to move between pages.
 
@@ -103,7 +140,7 @@ Modes:
 `
 
 // ParseFlags parses the command line options.
-func ParseFlags() (*Options, error) {
+func ParseFlags(app Applicator) (*Options, error) {
 	var options Options
 	var parser = flags.NewParser(&options, flags.HelpFlag)
 	parser.Usage = cmdTpl
@@ -113,7 +150,7 @@ func ParseFlags() (*Options, error) {
 		"serve",
 		"Serve content on disk with the provided config",
 		"The serve command starts the web server with the provided yaml configuration file.",
-		&ServeCommand{},
+		&ServeCommand{App: app},
 	)
 
 	// Add the 'init' command
@@ -121,7 +158,7 @@ func ParseFlags() (*Options, error) {
 		"init",
 		"Init a project",
 		"Initialise a project by writing the embedded demo project to disk.",
-		&InitCommand{},
+		&InitCommand{App: app},
 	)
 
 	// Add the 'demo' command
@@ -129,15 +166,13 @@ func ParseFlags() (*Options, error) {
 		"demo",
 		"Run the demo server",
 		"Serve the demo content embedded in the program.",
-		&ServeCommand{},
+		&DemoCommand{App: app},
 	)
 
 	// Catch errors in caller.
 	if _, err := parser.Parse(); err != nil {
 		return nil, err
 	}
-
-	fmt.Printf("%#v\n", options)
 
 	return &options, nil
 }
--- flags_test.go
+++ flags_test.go
@@ -9,7 +9,22 @@ import (
 	"github.com/jessevdk/go-flags"
 )
 
+// TestApplication implements the Applicator interface.
+type TestApplication struct{}
+
+func (t *TestApplication) Serve(address, port, configFile string) error {
+	return nil
+}
+func (t *TestApplication) Init(directory string) error {
+	return nil
+}
+func (t *TestApplication) Demo(address, port string) error {
+	return nil
+}
+
 func TestParseFlags(t *testing.T) {
+
+	testApp := &TestApplication{}
 
 	tests := []struct {
 		name string
@@ -17,35 +32,65 @@ func TestParseFlags(t *testing.T) {
 		err  error
 	}{
 		{
-			name: "serve: all options",
+			name: "serve all options",
 			args: []string{"program", "serve", "-a", "127.0.0.1", "-p", "8001", "config.yaml"},
 			err:  nil,
 		},
 		{
-			name: "serve: only config",
+			name: "serve only config",
 			args: []string{"program", "serve", "config.yaml"},
 			err:  nil,
 		},
 		{
-			name: "serve: no config",
+			name: "serve no config",
 			args: []string{"program", "serve", "--address", "127.0.0.2"},
 			err:  &flags.Error{},
 		},
 		{
-			name: "serve: invalid address",
+			name: "serve invalid address",
 			args: []string{"program", "serve", "-a", "hi", "-p", "8001", "config.yaml"},
 			err:  FlagCustomError{},
 		},
 		{
-			name: "serve: invalid port",
+			name: "serve invalid port",
 			args: []string{"program", "serve", "-a", "127.0.0.3", "-p", "eight", "config.yaml"},
+			err:  FlagCustomError{},
+		},
+		{
+			name: "init ok default",
+			args: []string{"program", "init"},
+			err:  nil,
+		},
+		{
+			name: "init ok with tmp dir",
+			args: []string{"program", "init", "-d", "/tmp"},
+			err:  nil,
+		},
+		{
+			name: "init failure",
+			args: []string{"program", "init", "-d", "/_DATA/tmp"},
+			err:  FlagCustomError{},
+		},
+		{
+			name: "demo ok",
+			args: []string{"program", "demo", "-a", "127.0.0.1", "-p", "8001"},
+			err:  nil,
+		},
+		{
+			name: "demo ok no args",
+			args: []string{"program", "demo"},
+			err:  nil,
+		},
+		{
+			name: "demo invalid address",
+			args: []string{"program", "demo", "-a", "url", "-p", "8001"},
 			err:  FlagCustomError{},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			os.Args = tt.args
-			_, err := ParseFlags()
+			_, err := ParseFlags(testApp)
 			if tt.err != nil {
 				if err == nil {
 					t.Fatal("expected an error")
