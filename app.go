d11deb22cb5c0d3fdcefe1cd5d86d692ba2464d7
rorycl <rorycl@campbell-lange.net>
2025-10-25 09:52:48 +0100

    app: add injectable app containing main entry points

---
+++ app.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"os"
+)
+
+// App is the main "plug point" for the application, making the two
+// modes of "Serve" (embedded and on disk) and "WriteAssets" injectable
+// into the cli flags package.
+type App struct {
+	serveFunc func(*server) error
+	writeFunc func(cfg *config, directory string) error
+}
+
+// NewApp returns a new App.
+func NewApp() *App {
+	return &App{
+		serveFunc: Serve,
+		writeFunc: WriteAssets,
+	}
+}
+
+// Serve serves the service from disk.
+func (a *App) Serve(address, port, configFile string) error {
+	configBytes, err := os.ReadFile(configFile)
+	if err != nil {
+		return err
+	}
+
+	config, err := newConfig(configBytes, false)
+	if err != nil {
+		return err
+	}
+
+	server, err := newServer(address, port, config)
+	if err != nil {
+		return err
+	}
+	return a.serveFunc(server)
+}
+
+// Demo serves the service from embedded assets.
+func (a *App) Demo(address, port string) error {
+	config, err := newConfig(configYaml, true) // is bytes
+	if err != nil {
+		return err
+	}
+
+	server, err := newServer(address, port, config)
+	if err != nil {
+		return err
+	}
+	return a.serveFunc(server)
+}
+
+// Init writes the internal directories and config to disk.
+func (a *App) Init(dir string) error {
+	config, err := newConfig(configYaml, true) // is bytes
+	if err != nil {
+		return err
+	}
+	return a.writeFunc(config, dir)
+}
---
+++ app_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+func TestApp(t *testing.T) {
+
+	ServeTestOK := func(*server) error {
+		return nil
+	}
+	ServeTestFail := func(*server) error {
+		return errors.New("fail")
+	}
+	WriteTestOK := func(cfg *config, directory string) error {
+		return nil
+	}
+	WriteTestFail := func(cfg *config, directory string) error {
+		return errors.New("fail")
+	}
+
+	appOK := NewApp()
+	appOK.serveFunc = ServeTestOK
+	appOK.writeFunc = WriteTestOK
+
+	appFail := NewApp()
+	appFail.serveFunc = ServeTestFail
+	appFail.writeFunc = WriteTestFail
+
+	// pass cases
+	err := appOK.Serve("127.0.0.1", "8000", "config.yaml")
+	if err != nil {
+		t.Fatalf("got unexepected error %v", err)
+	}
+	err = appOK.Demo("127.0.0.1", "8000")
+	if err != nil {
+		t.Fatalf("got unexepected error %v", err)
+	}
+	err = appOK.Init(os.TempDir())
+	if err != nil {
+		t.Fatalf("got unexepected error %v", err)
+	}
+
+	// error cases
+	err = appFail.Serve("127.0.0.1", "8000", "config.yaml")
+	if err == nil {
+		t.Fatal("unexpected success for app failure mode")
+	}
+	err = appFail.Demo("127.0.0.1", "8000")
+	if err == nil {
+		t.Fatal("unexpected success for app failure mode")
+	}
+	err = appFail.Init(os.TempDir())
+	if err == nil {
+		t.Fatal("unexpected success for app failure mode")
+	}
+
+}
