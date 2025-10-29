9b3e536310349221dd0db682e90411c86292b111
rorycl <rorycl@campbell-lange.net>
2025-10-29 10:41:01 +0000

    app: extend tests, add test file cleanup

--- app_test.go
+++ app_test.go
@@ -3,71 +3,328 @@ package main
 import (
 	"errors"
 	"os"
+	"strings"
 	"testing"
 )
 
+const testFilePattern = "firstgo_apptest_*"
+
+func writeConfig(t *testing.T, config []byte) string {
+	t.Helper()
+	tf, err := os.CreateTemp("", testFilePattern)
+	if err != nil {
+		t.Fatal("create temp error", err)
+	}
+	err = os.WriteFile(tf.Name(), config, 0600)
+	if err != nil {
+		t.Fatal("write temp error", err)
+	}
+	return tf.Name()
+}
+
+// makeOKConfig makes a temporary config file on disk or returns it as a
+// string.
+func makeOKConfig(t *testing.T, asPath bool) string {
+	t.Helper()
+	config := `
+---
+assetsDir: "assets"
+pageTemplate: "templates/page.html"
+indexTemplate: "templates/index.html"
+pages:
+  -
+    URL: "/home"
+    Title: "Home"
+    ImagePath: "images/home.jpg"
+    Zones:
+      -
+        Left:   367
+        Top:    44
+        Right:  539
+        Bottom: 263
+        Target: "/detail"
+  -
+    URL: "/detail"
+    Title: "Detail"
+    ImagePath: "images/detail.jpg"
+    Zones:
+      -
+        Left: 436
+        Top:  31
+        Right: 538
+        Bottom: 73
+        Target: "/home"
+`
+	if !asPath {
+		return config
+	}
+	return writeConfig(t, []byte(config))
+}
+
+// makeNotOKConfig makes a temporary, broken config file on disk or as a
+// string.
+func makeNotOKConfig(t *testing.T, asPath bool) string {
+	t.Helper()
+	config := `
+---
+assetsDir: "assets"
+pageTemplate: "templates/page.html"
+indexTemplate: "templates/index.html"
+pages:
+  -
+    URL: "/home"
+    Title: "Home"
+    ImagePath: "images/home.jpg"
+    Zones:
+      -
+        Left:   367
+        Top:    44
+        Right:  539
+        Bottom: 263
+        Target: "/detail"
+  -
+    URL: "/detail"
+    Title: "Detail"
+    ImagePath: "images/detail.jpg"
+    Zones:
+      -
+        Left: 436
+        Top:  31
+        Right: 538
+        Bottom: 73
+        Target: "/broken"  # a broken target
+`
+	if !asPath {
+		return config
+	}
+	return writeConfig(t, []byte(config))
+
+}
+
+// make a non existant config (ignore asPath condition, not relevant to
+// demo mode).
+func makeNotExistentConfig(t *testing.T, asPath bool) string {
+	filer := writeConfig(t, []byte(""))
+	_ = os.Remove(filer)
+	return filer
+}
+
 func TestApp(t *testing.T) {
 
-	ServeTestOK := func(*server) error {
-		return nil
-	}
-	ServeTestFail := func(*server) error {
-		return errors.New("fail")
-	}
-	WriteTestOK := func(cfg *config, directory string) error {
-		return nil
-	}
-	WriteTestFail := func(cfg *config, directory string) error {
-		return errors.New("fail")
+	tests := []struct {
+		name        string
+		mode        string
+		address     string
+		app         App
+		mkConfig    func(t *testing.T, asPath bool) string
+		errContains string
+	}{
+		{
+			name:    "interactive true serve ok",
+			mode:    "serve",
+			address: "127.0.0.1",
+			app: App{
+				interactive: true,
+				serveFunc:   func(*server) error { return nil },
+			},
+			mkConfig: makeOKConfig,
+		},
+		{
+			name: "interactive false serve ok",
+			mode: "serve",
+			app: App{
+				interactive: false,
+				serveFunc:   func(*server) error { return nil },
+			},
+			mkConfig: makeOKConfig,
+			address:  "127.0.0.1",
+		},
+		{
+			name: "interactive false serve fail config failure",
+			mode: "serve",
+			app: App{
+				interactive: false,
+				serveFunc:   func(*server) error { return nil },
+			},
+			mkConfig:    makeNotOKConfig,
+			address:     "127.0.0.1",
+			errContains: "invalid Zone Target URL",
+		},
+		{
+			name: "interactive false serve fail non existent config",
+			mode: "serve",
+			app: App{
+				interactive: false,
+				serveFunc:   func(*server) error { return nil },
+			},
+			mkConfig:    makeNotExistentConfig,
+			address:     "127.0.0.1",
+			errContains: "no such file or directory",
+		},
+		{
+			name: "interactive false serve fail address",
+			mode: "serve",
+			app: App{
+				interactive: false,
+				serveFunc:   func(*server) error { return nil },
+			},
+			mkConfig:    makeOKConfig,
+			address:     "nonsense",
+			errContains: "invalid IP address",
+		},
+		{
+			name: "interactive false serve fail ",
+			mode: "serve",
+			app: App{
+				interactive: false,
+				serveFunc:   func(*server) error { return errors.New("serve fail") },
+			},
+			mkConfig:    makeOKConfig,
+			address:     "127.0.0.1",
+			errContains: "serve fail",
+		},
+		{
+			name: "demo ok interactive",
+			mode: "demo",
+			app: App{
+				interactive: true,
+				serveFunc:   func(*server) error { return nil },
+			},
+			mkConfig: makeOKConfig,
+			address:  "127.0.0.1",
+		},
+		{
+			name: "demo ok",
+			mode: "demo",
+			app: App{
+				interactive: false,
+				serveFunc:   func(*server) error { return nil },
+			},
+			mkConfig: makeOKConfig,
+			address:  "127.0.0.1",
+		},
+		{
+			name: "demo serve failure",
+			mode: "demo",
+			app: App{
+				interactive: false,
+				serveFunc:   func(*server) error { return errors.New("demo serve failure") },
+			},
+			mkConfig:    makeOKConfig,
+			address:     "127.0.0.1",
+			errContains: "demo serve failure",
+		},
+		{
+			name: "demo fail config failure",
+			mode: "demo",
+			app: App{
+				interactive: false,
+				serveFunc:   func(*server) error { return nil },
+			},
+			mkConfig:    makeNotOKConfig,
+			address:     "127.0.0.1",
+			errContains: "invalid Zone Target URL",
+		},
+		{
+			name: "demo false serve fail address",
+			mode: "demo",
+			app: App{
+				interactive: false,
+				serveFunc:   func(*server) error { return nil },
+			},
+			mkConfig:    makeOKConfig,
+			address:     "nonsense",
+			errContains: "invalid IP address",
+		},
+		{
+			name: "init ok interactive",
+			mode: "init",
+			app: App{
+				interactive: true,
+				writeFunc:   func(cfg *config, directory string) error { return nil },
+			},
+			mkConfig: makeOKConfig,
+		},
+		{
+			name: "init ok",
+			mode: "init",
+			app: App{
+				interactive: false,
+				writeFunc:   func(cfg *config, directory string) error { return nil },
+			},
+			mkConfig: makeOKConfig,
+			address:  "127.0.0.1",
+		},
+		{
+			name: "init failure",
+			mode: "init",
+			app: App{
+				interactive: false,
+				writeFunc:   func(cfg *config, directory string) error { return errors.New("init failure") },
+			},
+			mkConfig:    makeOKConfig,
+			errContains: "init failure",
+		},
+		{
+			name: "init fail config failure",
+			mode: "init",
+			app: App{
+				interactive: false,
+				writeFunc:   func(cfg *config, directory string) error { return nil },
+			},
+			mkConfig:    makeNotOKConfig,
+			errContains: "invalid Zone Target URL",
+		},
 	}
 
-	appOK := NewApp()
-	appOK.serveFunc = ServeTestOK
-	appOK.writeFunc = WriteTestOK
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var err error
+			switch tt.mode {
+			case "serve":
+				cleanup := func(fileName string) func() {
+					return func() { _ = os.Remove(fileName) }
+				}
+				config := tt.mkConfig(t, true) // bool is for "asPath" mode
+				t.Cleanup(cleanup(config))
+				err = tt.app.Serve(tt.address, "8000", config)
+			case "demo":
+				config := tt.mkConfig(t, false) // config as string only
+				orig := configYaml
+				configYaml = []byte(config) // override embed
+				err = tt.app.Demo(tt.address, "8000")
+				configYaml = orig
+			case "init":
+				config := tt.mkConfig(t, false) // config as string only
+				orig := configYaml
+				configYaml = []byte(config) // override embed
+				err = tt.app.Init("anything goes")
+				configYaml = orig
+			default:
+				t.Fatalf("mode %q not known", tt.mode)
+			}
+			if err != nil {
+				if tt.errContains == "" {
+					t.Fatal("unexpected error", err)
+				}
+				if got, want := err.Error(), tt.errContains; !strings.Contains(got, want) {
+					t.Errorf("got err %q want err with %q", got, want)
+				}
+			}
+			if err == nil && tt.errContains != "" {
+				t.Fatalf("expected err with %q", tt.errContains)
+			}
+		})
+	}
+}
 
-	appFail := NewApp()
-	appFail.serveFunc = ServeTestFail
-	appFail.writeFunc = WriteTestFail
-
-	// pass cases
-	err := appOK.Serve("127.0.0.1", "8000", "config.yaml")
-	if err != nil {
-		t.Fatalf("got unexepected error %v", err)
+func TestAppNewInteractive(t *testing.T) {
+	app := NewApp()
+	if app.interactive != false {
+		t.Fatal("expected interactive field to be false")
 	}
-	err = appOK.Demo("127.0.0.1", "8000")
-	if err != nil {
-		t.Fatalf("got unexepected error %v", err)
+	app.Interactive()
+	if app.interactive != true {
+		t.Fatal("expected interactive field to be true")
 	}
-	err = appOK.Init(os.TempDir())
-	if err != nil {
-		t.Fatalf("got unexepected error %v", err)
-	}
-
-	// error cases
-	err = appFail.Serve("127.0.0.1", "8000", "config.yaml")
-	if err == nil {
-		t.Fatal("unexpected success for app failure mode")
-	}
-	err = appFail.Demo("127.0.0.1", "8000")
-	if err == nil {
-		t.Fatal("unexpected success for app failure mode")
-	}
-	err = appFail.Init(os.TempDir())
-	if err == nil {
-		t.Fatal("unexpected success for app failure mode")
-	}
-
-	// interactive
-	if got, want := appOK.interactive, false; got != want {
-		t.Errorf("interactive status got %t want %t", got, want)
-	}
-	appOK.Interactive()
-	if got, want := appOK.interactive, true; got != want {
-		t.Errorf("interactive status after switch got %t want %t", got, want)
-	}
-	appOK.Interactive()
-	if got, want := appOK.interactive, false; got != want {
-		t.Errorf("interactive status after second switch got %t want %t", got, want)
-	}
-
 }
